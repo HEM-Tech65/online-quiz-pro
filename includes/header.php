1c84c9ea055e05a1c3c646f3dd996dad73fc73fc
HEM-Tech65 <hutchisonmax65@yahoo.com>
2025-07-07 15:58:05 +0000

    fixed the dropdown button of the header

--- includes/header.php
+++ includes/header.php
@@ -27,10 +27,7 @@ $current_page = basename($_SERVER['PHP_SELF']);
 
     <script>document.documentElement.className += 'js-enabled';</script>
 
-    <!-- Bootstrap 5 JS Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-    <!-- Font Awesome JS -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/js/all.min.js"></script>
+   
 </head>
 <body>
     <!-- Navigation -->
